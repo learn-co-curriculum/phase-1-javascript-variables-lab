d8401b1f1bc420a545d2f3284951b9992c809197
Gabe Jackson <gj@mail.co.de>
2017-09-25 15:58:52 -0400

    Add solution

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-let mostProfitableNeighborhood = 'Chelsea'
-const companyName = 'Scuber'
-let companyCeo = 'Susan Smith'
+// Code your solution in this file!
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
