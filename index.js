ddf8b068d2a294af5ec2a873383f0e9b13cf8c55
Hpollard003 <hpollard003@gmail.com>
2021-03-16 13:31:31 -0400

    commit tree 4f562aec7cc66ba88f510b70630bdffcf54b38cc

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+// companyName will allways be Scuber from this point on
+
+let mostProfitableNeighborhood = "Chelsea"
+//let variable will give me an error if i try to re-define it
+
+let companyCeo = "Susan Smith"
+//Susan runs that shit
+
+
+
