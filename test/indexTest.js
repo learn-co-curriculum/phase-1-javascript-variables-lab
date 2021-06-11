8db698b80c1350b2c251556fd656fbcae2ce8629
ihollander <ianholla@gmail.com>
2021-06-11 07:55:07 -0400

    Removed file-system import

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,14 +1,3 @@
-const chai = require('chai')
-const expect = chai.expect;
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
-
-const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-
-
-
 describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber', function () {
