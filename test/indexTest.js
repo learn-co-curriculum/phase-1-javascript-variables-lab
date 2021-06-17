1edaaddaf00613a767767e32ce243beb324c46e2
Liz Burton <liz.burton147@gmail.com>
2021-06-17 17:00:20 -0400

    Add missing variables

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
 describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber', function () {
