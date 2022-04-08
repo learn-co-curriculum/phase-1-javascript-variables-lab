f5616e4825ec82265fefae62cc81d027b10d806d
Njoki <kinuthiash@gmail.com>
2022-04-08 12:11:21 +0300

    fix:edit companyCeo variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
-const companyName = 'Scuber'
+const companyName = 'Scuber';
 
-let mostProfitableNeighborhood = 'Chelsea'
-// mostProfitableNeighborhood === 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
