78410f3b9b13a1fe91209d0c0df50db666bcba75
s-k3nyyy <collo.c.heg@gmail.com>
2024-03-20 09:49:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const myName = 'collins njuguna';
+console.log(myName);
+
+
+let myName2 = 'collins njuguna';
+console.log(myName2);
+
+
+myName2 = 'collins';
+console.log(myName2);
